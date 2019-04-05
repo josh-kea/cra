97f9849893e170a652b557a9f08cae1453ec0c0c
josh-kea <josh0661@stud.kea.dk>
2019-04-05 22:19:27 +0200

    i finally understand what props is

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+const Footer = () => {
+    return <footer>&copy; 2019</footer>
+};
 
+function App(){
+    return (
+        <div id="App">
+            <Header />
+            <Header />
+            <Header />
+            <Person name="JOSHUA!!!!!!"></Person>
+            <Person name="Nek Minnit"/>
+            <Footer />
+        </div>
+    );
+}
+
+function Header(){
+        return (
+            <header>
+                <h1>This is a header</h1>
+            </header>
+        )
+}
+
+function Person(props){
+    return (
+        <article>
+            <h1>{props.name}</h1>
+            <p>Hi Mother!</p>
+        </article>
+    );
+}
+
+
+//JSX, we are mixing javascript and HTML --!!
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
